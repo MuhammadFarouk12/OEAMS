d7d6e216837698aa80351b2384b9e186db2b04e8
MuhammadFarouk12 <Muhammad12fk@gmail.com>
2025-08-05 19:16:55 +0300

    Initial commit

---
+++ Landing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OEAMS
+{
+    public partial class Landing : Form
+    {
+        public static string connectionString = "Server=sql12.freesqldatabase.com;Port=3306;Database=sql12793688;Uid=sql12793688;Pwd=myALH6KclI";
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        private void Landing_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
